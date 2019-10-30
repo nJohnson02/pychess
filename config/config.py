2ee0ef2274a48d4a2ab1db8e88d96c4d94fb74a1
Ryan Harrington <rharrington.web@gmail.com>
2019-10-29 19:09:35 -0600

    PyGame Setup
    Remvoed TKinter code

--- config/config.py
+++ config/config.py
@@ -1,1 +1,49 @@
+# Chess board alternating color
 boardCol = '#2e3947'
+
+# Piece setup coordinates
+pieces = {  
+    'blackBishop.png': [
+        (0, 2), (0, 5)
+    ], 
+    'blackKing.png': [
+        (0, 4)
+    ], 
+    'blackKnight.png': [
+        (0, 1), (0, 6)
+    ], 
+    'blackPawn.png': [
+        (1, 0), (1, 1), 
+        (1, 2), (1, 3), 
+        (1, 4), (1, 5), 
+        (1, 6), (1, 7)
+    ],
+    'blackQueen.png': [
+        (0, 3)
+    ], 
+    'blackRook.png': [
+        (0, 0), (0, 7)
+    ], 
+    'whiteBishop.png': [
+        (7, 2), (7, 5)
+    ], 
+    'whiteKing.png': [
+        (7, 4)
+    ], 
+    'whiteKnight.png': [
+        (7, 1), (7, 6)
+    ], 
+    'whitePawn.png': [
+        (6, 0), (6, 1), 
+        (6, 2), (6, 3), 
+        (6, 4), (6, 5), 
+        (6, 6), (6, 7)
+    ], 
+    'whiteQueen.png': [
+        (7, 3)
+    ], 
+    'whiteRook.png': [
+        (7, 0), (7, 7)
+    ]
+}
+
--- main.py
+++ main.py
@@ -1,48 +1,20 @@
 import os
 import sys
-from tkinter import *
+from pygame import *
 
 sys.path.insert(0, 'src')
 from src import movement, pieces
 from config import config
 
-win = Tk()
-
 # Setup
-win.wm_iconbitmap('assets/blackKnight.ico')
-win.wm_title('Chess')
 
 class Board:
 
     def __init__(self, win):
 
-        self.coords = []
-
-        boardFrame = Frame(win)
-        boardFrame.grid()
-
-        blank = PhotoImage(file='assets/pieces/blank.png')
-
-        for r in range(8):
-
-            for c in range(8):
-
-                btn = Button(boardFrame, image=blank, width=75, height=75, borderwidth=0)
-                btn.configure(background='#fff')
-                btn.image = blank
-
-                if ((r % 2 == 0) & (c % 2 == 0)) | (r % 2 == c % 2):
-                    btn.configure(background=config.boardCol)
-
-                btn.grid(row=r, column=c)
-
-                self.coords.append(pieces.Coordinate((r, c), btn, blank))
+        pass
 
     def setBoard(self):
         
         pass
-
-board = Board(win)
-
-win.mainloop()
 
