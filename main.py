76db3135eee7c5131cc96a0bf699b452fc612576
Ryan Harrington <rharrington.web@gmail.com>
2019-10-29 21:03:05 -0600

    Window Initialization

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import os
 import sys
 import pygame
-from pygame import *
+from pygame.locals import *
 
 sys.path.insert(0, 'src')
-from src import movement, pieces
-from config import config
+from src import *
+from config import *
 
 # Setup
-screen = pygame.display.set_mode((800, 800))
+pygame.init()
+ 
+fps = 60
+fpsClock = pygame.time.Clock()
+ 
+width, height = 1200, 720
+screen = pygame.display.set_mode((width, height))
+
+
 class Board:
 
     def __init__(self, win):
@@ -18,4 +26,19 @@ class Board:
     def setBoard(self):
         
         pass
+
+
+while True:
+
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Update.
+
+    pygame.display.flip()
+    fpsClock.tick(fps)
 
